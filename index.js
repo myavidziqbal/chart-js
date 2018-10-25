ac0cea7c0c67f03307ff6869a36c843b6fe76eaa
myavidziqbal <sademark08@gmail.com>
2018-10-25 14:17:51 +0700

    Tester

---
+++ index.js
@@ -0,0 +1,15 @@
+function addPoduct() {
+    
+}
+
+
+class Cart {
+    constructor(colorItem, countItem){
+    this.colorItem = colorItem
+    this.countItem = countItem
+    }
+}
+
+const addProduct = new Cart("White Cap", 2 )
+
+console.log(addProduct);
